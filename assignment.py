c01861d42be06467a2c0619b22aa6da1d6c1e26e
chidex774 <chideradavea@gmail.com>
2025-12-01 07:02:54 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,20 @@
+# Physics formulas by variable
+
+var = input("Enter a variable (a, b, c, d, e): ")
+
+if var == "a":
+    print("Momentum = a * c")
+
+elif var == "b":
+    print("Force = a * b")
+elif var == "c":
+    print("Kinetic Energy = 0.5 * a * c^2")
+
+elif var == "d":
+    print("Work = (a * b) * d")
+
+elif var == "e":
+    print("Potential Energy = a * d * e")
+
+else:
+    print("Invalid input. Please enter a, b, c, d, or e.")
